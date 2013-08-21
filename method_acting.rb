f3b53596322df37ff08cf5b21550f90d4588d39c
Szuturon <ronnyszutu@gmail.com>
2013-08-21 09:26:00 -0400

    finished methods

---
+++ method_acting.rb
@@ -0,0 +1,17 @@
+return __callee__
+
+def dogs
+  ___
+end
+
+def cats
+  ___
+end
+
+def chunky_bacon
+  ___
+end
+
+assert_equal dogs, :dogs
+assert_equal cats, :cats
+assert_equal chunky_bacon, :chunky_bacon
